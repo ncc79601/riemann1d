85befbf62179f1213aa3b8444657bbf9377a0c40
NCC79601 <ncc79601@163.com>
2026-05-18 01:02:38 +0800

    docs: basic implementation of Documenter.jl

---
+++ docs/make.jl
@@ -0,0 +1,27 @@
+import Pkg
+Pkg.activate(@__DIR__)
+Pkg.develop(path=joinpath(@__DIR__, ".."))
+
+using Riemann1D
+using Documenter
+
+DocMeta.setdocmeta!(Riemann1D, :DocTestSetup, :(using Riemann1D); recursive=true)
+
+makedocs(
+    modules = [Riemann1D],
+    authors = "NCC79601 <ncc79601@163.com>",
+    sitename = "Riemann1D.jl",
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", "false") == "true",
+        collapselevel = 1,
+    ),
+    pages = [
+        "Home" => "index.md",
+        "API Reference" => "api.md",
+    ],
+)
+
+deploydocs(
+    repo = "github.com/NCC79601/Riemann1D.git",
+    devbranch = "dev",
+)
